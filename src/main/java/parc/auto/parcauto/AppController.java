f9ee55b4de7777980547580ec342d81ca9f3742a
alexandru-drincianu <alexdrincianu@gmail.com>
2021-10-26 11:03:48 +0300

    Custom Login

--- src/main/java/parc/auto/parcauto/AppController.java
+++ src/main/java/parc/auto/parcauto/AppController.java
@@ -1,6 +1,9 @@
 package parc.auto.parcauto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -35,5 +38,14 @@ public class AppController {
         repo.save(user);
 
         return "register_success";
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "login";
+        }
+        return "redirect:/";
     }
 }
